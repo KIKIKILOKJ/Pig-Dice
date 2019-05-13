811e67946a8ec9f647c3911c394887da4c72d2b8
peter max <petermax2004@gmail.com>
2019-05-13 09:44:11 +0300

    Add the codes to js file so as to allow the game to achieve functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,44 @@ $(document).ready(function(){
     })
 });
 
-var total = 0;
+var throwdice = function () {
+  return Math.floor(6*Math.random ()) + 1;
+}
 
-function anyNumber(){
-var randomNumber = math.floor((math.random() *6) +1);
-return randonNumber;
+function Player (turn) {
+  this.roll = 0;
+  this.score = 0;
+  this.total = 0;
+  this.turn =0;
+}
+
+Player.prototype.roll1 = function () {
+  if (this.roll===1) {
+    this.score = 0;
+    this.changeturn ();
+  }
+  else {
+    this.score += this.roll
+  }
+}
+
+Player.prototype.hold = function (){
+  this.score += this.score;
+  this.total =o;
+  this.changeturn ();
+}
+
+Player.prototype.changeturn = function (){
+  if(this.roll===1) {
+    this.turn = false;
+  }
+  else {
+    this.turn = true;
+  }
+}
+
+Player.prototype.winnerCheck = function () {
+  if(this.totalscore>=100) {
+    alert(" You are the winner!!");
+  }
 }
